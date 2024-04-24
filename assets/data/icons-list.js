7429aa94f94537befc0ea96f71141f21e084bf87
Victor Araujo <victor_emanuel9@hotmail.com>
2024-04-23 17:54:20 -0700

    chore: remove all unrelated data

---
+++ assets/data/icons-list.js
@@ -0,0 +1,32 @@
+const knowledgeIcons = [
+  {
+    title: "Git",
+    src: "https://www.freeiconspng.com/uploads/github-logo-icon-10.jpg",
+    alt: "Git Logo",
+  },
+  {
+    title: "Github",
+    src: "https://www.freeiconspng.com/uploads/github-cat-in-a-circle-icon-21.png",
+    alt: "Github Logo",
+  },
+  {
+    title: "Linux",
+    src: "https://www.freeiconspng.com/uploads/penguin-linux-cartoon-png-17.png",
+    alt: "Linux Logo",
+  },
+  {
+    title: "Javascript",
+    src: "https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png",
+    alt: "Javascript Logo",
+  },
+  {
+    title: "HTML5",
+    src: "https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-logo-0.png",
+    alt: "HTML5 Logo",
+  },
+  {
+    title: "CSS3",
+    src: "https://img.icons8.com/color/480/000000/css3.png",
+    alt: "CSS3 Logo",
+  },
+];
