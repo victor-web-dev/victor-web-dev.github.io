ee5e3b7533ce2d3d8d1f030807c5a4cbcfea1978
Victor Araujo <victor_emanuel9@hotmail.com>
2024-05-02 17:48:36 -0700

    change: icons list

---
+++ assets/data/icons-list.js
@@ -0,0 +1,39 @@
+export const MostUsedIcons = [
+  {
+    title: "Git",
+    src: "https://img.icons8.com/color/96/git.png",
+    alt: "Git Logo",
+  },
+  {
+    title: "Linux",
+    src: "https://img.icons8.com/color/96/linux--v1.png",
+    alt: "Linux Logo",
+  },
+  {
+    title: "HTML5",
+    src: "https://img.icons8.com/dusk/64/html-5.png",
+    alt: "HTML5 Logo",
+  },
+  {
+    title: "CSS3",
+    src: "https://img.icons8.com/dusk/64/css3.png",
+    alt: "CSS3 Logo",
+  },
+  {
+    title: "Javascript",
+    src: "https://img.icons8.com/color/48/javascript--v1.png",
+    alt: "Javascript Logo",
+  },
+  {
+    title: "Typescript",
+    src: "https://img.icons8.com/fluency/48/typescript--v1.png",
+    alt: "Typescript Logo",
+  },
+  {
+    title: "React",
+    src: "https://img.icons8.com/bubbles/100/react.png",
+    alt: "React Logo",
+  },
+];
+
+// module.exports = MostUsedIcons;
