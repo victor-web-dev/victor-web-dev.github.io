f0597bedf1dfe26dac0c77154e562dce84d503a9
Victor Araujo <victor_emanuel9@hotmail.com>
2024-05-02 17:50:04 -0700

    feat: add function

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-const checkbox = document.querySelector(".menu-checkbox");
-const navItem = document.querySelectorAll(".nav-item");
+// const { MostUsedIcons: usedIcons } = require("./assets/data/icons-list");
+import { MostUsedIcons as usedIcons } from "./assets/data/icons-list.js";
 
-checkbox.addEventListener("change", (event) => {
-  if (!event.target.checked) {
-    navItem.forEach((element) => (element.className = "nav-item display-none"));
-  } else {
-    navItem.forEach((element) => (element.className = "nav-item"));
-  }
-});
+const navBtnClick = () => {
+  const checkbox = document.querySelector(".menu-checkbox");
+  const navItem = document.querySelectorAll(".nav-item");
+
+  checkbox.addEventListener("change", (event) => {
+    if (!event.target.checked) {
+      navItem.forEach(
+        (element) => (element.className = "nav-item display-none")
+      );
+    } else {
+      navItem.forEach((element) => (element.className = "nav-item"));
+    }
+  });
+};
+
+const fillUpTechDiv = () => {
+  const mostDiv = document.getElementById("most-used-tech");
+  // const lessDiv = document.getElementById("less-used-tech");
+  usedIcons.forEach((el) => {
+    mostDiv.innerHTML += `
+    <img class="logo" src="${el.src}" title="${el.title}" alt="${el.alt}"/>
+    `;
+  });
+};
+
+const exec = () => {
+  navBtnClick();
+  fillUpTechDiv();
+};
+
+exec();
